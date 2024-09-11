ef773c7c18c27da21b4cd59ce44c50d13b590aa8
Yulike Dwi Nurcahyani <129092675+YikeCahya@users.noreply.github.com>
2024-09-11 12:39:01 +0700

    Add files via upload

---
+++ PraktikumDasPro-Jobsheet1/Latihan28.java
@@ -0,0 +1,29 @@
+public class Latihan28 {
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Yulike Dwi Nurcahyani");
+        //System.out.print("\nsaya adalah Mahasiswa Prodi Teknik Informatika");
+
+
+
+
+
+
+
+
+
+
+
+
+        //System.out.println("-------------------------------------------------------");
+        //System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+        //System.out.println("-------------------------------------------------------");
+        //System.out.println("Nama      : Yulike Dwi Nurcahyani");
+        //System.out.println("NIM       : 244107020146");
+        //System.out.println("Kelas     : 1A");
+        //System.out.println("No Absen  : 28");
+        //System.out.println("Prodi     : D-IV Teknik Informatika");
+        //System.out.println("Alamat    : Jl.Brigjen Katamso No.195 Kota Blitar / Jl. Senggani No. 18 Kota Malang");
+        //System.out.println("No HP     : 0895801012492");
+        //System.out.println("Hobi      : Jalan-jalan");
+    }
+}
